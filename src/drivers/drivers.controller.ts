c496413693e54ace56c769e99450c32243426c13
Petrik Diak <diak@petrik.hu>
2024-01-26 13:08:38 +0100

    ID végpontok elkapása

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { DriversService } from './drivers.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
@@ -18,17 +18,32 @@ export class DriversController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.driversService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    try{
+      return await this.driversService.findOne(+id);
+    }
+    catch{
+      throw new NotFoundException("Nincs ilyen ID!")
+    }
+
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDriverDto: UpdateDriverDto) {
-    return this.driversService.update(+id, updateDriverDto);
+  async update(@Param('id') id: string, @Body() updateDriverDto: UpdateDriverDto) {
+    try{
+      return await this.driversService.update(+id, updateDriverDto);
+    }
+    catch{
+      throw new NotFoundException("Nincs ilyen ID!")
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.driversService.remove(+id);
+  async remove(@Param('id') id: string) {
+    try{
+    return await this.driversService.remove(+id);
+  }catch{
+    throw new NotFoundException("Nincs ilyen ID!")
+  }
   }
 }
